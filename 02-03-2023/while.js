9ab35c13a44ea7af930fa4c69e992d276082765b
AndersonNazario <106996877+AndersonNazario@users.noreply.github.com>
2023-03-06 16:09:47 -0300

    exemplo de WHILE

---
+++ 02-03-2023/while.js
@@ -0,0 +1,8 @@
+const nomesDePaises = ['Brasil', 'Argentina', 'Bolivia', 'Uruguai', 'Peru'];
+
+let indice = 0;
+
+while (indice < nomesDePaises.length) {//enquanto
+    console.log(nomesDePaises[indice]);
+    indice++;
+}
