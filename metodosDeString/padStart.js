8a6d2a239eba1233856d7ca0fd7fff100525b1bb
AndersonNazario <106996877+AndersonNazario@users.noreply.github.com>
2023-04-03 21:59:24 -0300

    padStart

--- metodosDeString/padStart.js
+++ metodosDeString/padStart.js
@@ -1,5 +1,17 @@
 const ultimos4Digitos = "1234";
 
-const numeroCartao = ultimos4Digitos.padStart(19, "**** ");
+const numeroCartao = ultimos4Digitos.padStart(19, "**** "); //acresenta caracter 
 console.log(ultimos4Digitos);
 console.log(numeroCartao);
+
+//ex:
+console.log("___________________________");
+function imprimirData(dia, mes, ano) {
+    let diaFormatado = String(dia).padStart(2, "0");
+    let mesFormatado = `${mes}`.padStart(2, "0");
+
+    console.log(`${diaFormatado}/${mesFormatado}/${ano}`);
+
+}
+
+imprimirData(14, 1, 2021);
