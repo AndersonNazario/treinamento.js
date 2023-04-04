1a5bd8da9f0c99cabeb591f549a2feba6205d64e
AndersonNazario <106996877+AndersonNazario@users.noreply.github.com>
2023-04-03 21:18:46 -0300

    replace

--- metodosDeString/replace.js
+++ metodosDeString/replace.js
@@ -1,3 +1,14 @@
-const texto = "Eu amo jogar bola, amo fazer gol!";
+const texto = "Eu amo jogar bola, amo fazer gol! a cada gol eu ganho 97.50";
 
 console.log(texto.replace("amo", "adoro"));// troca as palavras informadas
+console.log(texto.replace(".", ","));
+
+/// ex 02
+console.log("_______________________________")
+let numero = "1,350,00";
+
+while (numero !== numero.replace(",", ".")) {
+    numero = numero.replace(",", ".");
+}
+
+console.log(numero);
