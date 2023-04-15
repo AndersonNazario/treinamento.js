f9f1302cfdb5507961550fef12afd055a39c153e
AndersonNazario <106996877+AndersonNazario@users.noreply.github.com>
2023-04-15 14:15:41 -0300

    Media aritimetica

---
+++ hackerrank/mediaAritimetica.js
@@ -0,0 +1,13 @@
+let valor = [1, 2, 3, 4];
+
+function solucao(lista) {
+    let soma = 0;
+    if (lista.length < 1000) {
+        for (let i = 0; i < lista.length; i++) {
+            soma += lista[i];
+        }
+    }
+    console.log(soma / lista.length);
+}
+
+solucao(valor);
