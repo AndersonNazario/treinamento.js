8030210d5cf53315d9c836bc92046389c5852851
AndersonNazario <106996877+AndersonNazario@users.noreply.github.com>
2023-04-18 23:06:10 -0300

    find pesquisa de carro

---
+++ metodosDeArraysII/ex_find.js
@@ -0,0 +1,16 @@
+const modelosCarro = [
+    { marca: 'fiat', ano: 2000, cor: 'Azul', porta: 5 },
+    { marca: 'fiat', ano: 2018, cor: 'Branco', porta: 5 },
+    { marca: 'Ferrari', ano: 2022, cor: 'Vermelha', porta: 3 },
+    { marca: 'Tesla', ano: 2023, cor: 'Preto', porta: 5 }
+];
+
+const validarCarro = (marcaDocarro, marcas) => {
+    return marcaDocarro.find((modelosCarro) => {
+        return modelosCarro.marca === marcas;
+    })
+};
+
+
+const resultadoPesquisa = validarCarro(modelosCarro, 'Ferrari');
+console.log(resultadoPesquisa);
