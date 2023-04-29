bfb395810a687c35605c6abeb13226dfad68a761
AndersonNazario <106996877+AndersonNazario@users.noreply.github.com>
2023-04-29 00:25:28 -0300

    setInterval

--- Callbacks/setInterval.js
+++ Callbacks/setInterval.js
@@ -1,10 +1,12 @@
 let numero = 1;
-nome(() => {
 
-})
-
-setInterval((nome) => {
+const contador = (nome) => {
+    if (numero >= 10) {
+        clearInterval(idSetInterval);
+    }
     console.log(`${numero} ${nome}`);
     numero++
-}, 2000);
+}
 
+const idSetInterval = setInterval(contador, 2000, 'Anderson');
+//1 Anderson, 2 Anderson ... 6 Anderson ..
