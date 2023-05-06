d9102e8fbb867177b58b925a743261e81a6917ec
AndersonNazario <106996877+AndersonNazario@users.noreply.github.com>
2023-05-06 18:03:14 -0300

    Organizacao

---
+++ MODULO-02/exercicios-backend-rotas-intermediarios-controladores/01/src/bancoDeDados.js
@@ -0,0 +1,8 @@
+const professores = [
+    { id: 1, nome: 'Anderson', stack: 'Backend' },
+    { id: 2, nome: 'Igor', stack: 'Backend' },
+    { id: 3, nome: 'Andrei', stack: 'Frontend' },
+    { id: 4, nome: 'Ana', stack: 'Backend' },
+];
+
+module.exports = professores;
--- MODULO-02/exercicios-backend-rotas-intermediarios-controladores/01/src/index.js
+++ MODULO-02/exercicios-backend-rotas-intermediarios-controladores/01/src/index.js
@@ -1,32 +1,11 @@
+const { filtrarProfessores, encontrarUmProfessor } = require('./controladores/professores');
 const express = require('express');
 const app = express();
 
-const professores = [
-    { id: 1, nome: 'Anderson', stack: 'Backend' },
-    { id: 2, nome: 'Igor', stack: 'Backend' },
-    { id: 3, nome: 'Andrei', stack: 'Frontend' },
-    { id: 4, nome: 'Ana', stack: 'Backend' },
-];
 // localhost:3000/professores
-app.get('/professores', (requisicao, resposta) => {
-    //console.log(requisicao.query); //parametro de consulta
-    //http://localhost:3000/professores/?stack=Backend  (Insomnia)
-    const { stack } = requisicao.query;
-    let resultado = professores;
-    if (stack) {
-        resultado = professores.filter((professor) => {
-            return professor.stack === stack;
-        });
-    }
+app.get('/professores', filtrarProfessores);
 
-    resposta.send(resultado);
-});
 // localhost:3000/professores/:id
-app.get('/professores/:id', (requisicao, resposta) => {
-    const profssorEncontrado = professores.find((professor) => {
-        return professor.id === Number(requisicao.params.id);
-    });
-    resposta.send(profssorEncontrado);
-});
+app.get('/professores/:id', encontrarUmProfessor);
 
 app.listen(3000);
