a4f145f5df803a0a8dde2d090cd58c2fe3404e33
AndersonNazario <106996877+AndersonNazario@users.noreply.github.com>
2023-05-11 17:20:33 -0300

    Adicionado atulização de instrutor

--- MODULO-02/api-rest/PreparacaoDoProjeto/src/controladores/instrutores.js
+++ MODULO-02/api-rest/PreparacaoDoProjeto/src/controladores/instrutores.js
@@ -1,10 +1,11 @@
 const bancoDeDados = require('../bancoDeDados');
 let { identificadorInstrutor } = require('../bancoDeDados');
 
+//ver listagem da coleção
 const listarInstrutores = (req, res) => {
     return res.json(bancoDeDados.instrutores)
 };
-
+//ver um recurso especifico da coleção
 const obterInstrutor = (req, res) => {
     const { id } = req.params;
 
@@ -18,7 +19,7 @@ const obterInstrutor = (req, res) => {
 
     return res.status(200).json(instrutor);
 }
-
+//adicionar recurso inteiro dentro de uma coleção
 const cadastrarInstrutor = (req, res) => {
     const { nome, email, status } = req.body;
 
@@ -41,9 +42,55 @@ const cadastrarInstrutor = (req, res) => {
 
     return res.status(201).json(instrutor);
 }
+//editar todo o recurço especifico
+const editarInstrutor = (req, res) => {
+    const { id } = req.params;
+    const { nome, email, status } = req.body;
+
+    if (!nome) {
+        return res.status(400).json({ mensagem: `Nome é obrigatoriso` });
+    }
+    if (!email) {
+        return res.status(400).json({ mensagem: `E-mail é obrigatorio!` });
+    }
+
+
+    const instrutor = bancoDeDados.instrutores.find((idIsntrutor) => {
+        return idIsntrutor.id === Number(id);
+    });
+
+    if (!instrutor) {
+        return res.status(404).json({ mensagem: 'instrutor não encontrado.' })
+    }
+
+    instrutor.nome = nome;
+    instrutor.email = email;
+    instrutor.status = status;
+
+    return res.status(204).json({ mensagem: `Atualizado` });
+}
+//editar uma parte do recurço escolhido
+const atualizarStatusDoIntrutor = (req, res) => {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    const instrutor = bancoDeDados.instrutores.find((idIsntrutor) => {
+        return idIsntrutor.id === Number(id);
+    });
+
+    if (!instrutor) {
+        return res.status(404).json({ mensagem: 'instrutor não encontrado.' })
+    }
+
+    instrutor.status = status;
+
+    return res.status(204).json();
+}
 
 module.exports = {
     listarInstrutores,
     obterInstrutor,
     cadastrarInstrutor,
+    editarInstrutor,
+    atualizarStatusDoIntrutor,
 };
--- MODULO-02/api-rest/PreparacaoDoProjeto/src/rotas.js
+++ MODULO-02/api-rest/PreparacaoDoProjeto/src/rotas.js
@@ -5,8 +5,10 @@ const rotas = express();
 const instrutores = require('./controladores/instrutores');
 
 
-rotas.get('/instrutores', instrutores.listarInstrutores);
-rotas.get('/instrutores/:id', instrutores.obterInstrutor);
-rotas.post('/instrutores', instrutores.cadastrarInstrutor);
+rotas.get('/instrutores', instrutores.listarInstrutores);//ver listagem da coleção
+rotas.get('/instrutores/:id', instrutores.obterInstrutor);//ver um recurso especifico da coleção
+rotas.post('/instrutores', instrutores.cadastrarInstrutor);//adicionar recurso dentro de uma coleção
+rotas.put('/instrutores/:id', instrutores.editarInstrutor);//editar todo o recurço especifico
+rotas.patch('/instrutores/:id/status', instrutores.atualizarStatusDoIntrutor);//editar uma parte do recurço escolhido
 
 module.exports = rotas;
